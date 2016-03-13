4c36e19ddc69fbfd5e23b69105c2d096789ceee8
titkonen <toni.itkonen@icloud.com>
2016-03-13 21:10:09 +0200

    Small unit converter.

--- UnitConverter/ViewController.swift
+++ UnitConverter/ViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Properties
+    @IBOutlet weak var tempText: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func textFieldReturn(sender: AnyObject) {
+        sender.resignFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        tempText.endEditing(true)
+    }
 
+    // Actions
+    @IBAction func convertTemp(sender: AnyObject) {
+        if let fahrenheit = Double(tempText.text!) {
+            let celsius = (fahrenheit - 32)/1.8
+            let resultText = "Celsius \(celsius)"
+            resultLabel.text = resultText
+        }
+    }
 
 }
 
